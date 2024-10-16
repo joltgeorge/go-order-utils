e9ad0eb64f371249e6304af98047e9ca235380d3
george miller <george.miller00404@gmail.com>
2024-10-17 09:45:44 +1100

    update the neg risk related contract address v2

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -30,8 +30,8 @@ var (
 	MATIC_CONTRACTS = &Contracts{
 		Exchange:         common.HexToAddress("0xbb82b482bc4a7e159759f184c724ae76561a680f"),
 		FeeModule:        common.HexToAddress("0x56C79347e95530c01A2FC76E732f9566dA16E113"),
-		NegRiskExchange:  common.HexToAddress("0xd2f980127e88c6f55d2dbbabccbffc9ff14f5340"),
-		NegRiskFeeModule: common.HexToAddress("0xf12c16fe6d1f9db269541ac62cf224925306715a"),
+		NegRiskExchange:  common.HexToAddress("0x4f8dffd9c1860b3f41f2f51828549371fc243516"),
+		NegRiskFeeModule: common.HexToAddress("0x43732487c768ce27160f81d5bd553371b30b7682"),
 		NegRiskAdapter:   common.HexToAddress("0x3ebcfdfc53f249ced0a7ec0d74647b1a6cc32471"),
 		Collateral:       common.HexToAddress("0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"),
 		Conditional:      common.HexToAddress("0x4D97DCd97eC945f40cF65F87097ACe5EA0476045"),
